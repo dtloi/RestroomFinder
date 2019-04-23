a07ec604d793070416b966566451a9f63016bf5f
Daniel Loi <dtloi@ucsc.edu>
2019-04-22 20:31:19 -0700

    Added basic UI components Added Map Activity In the process of setting up the Map

--- app/src/main/java/com/example/cmps121bdd/restroomfinder/MainActivity.java
+++ app/src/main/java/com/example/cmps121bdd/restroomfinder/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.cmps121bdd.restroomfinder;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import static com.example.cmps121bdd.restroomfinder.R.id.lookupLocation;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    boolean mLocationPermissionGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText inputLocation = findViewById(R.id.inputLocation);
+        Button lookupLocation = findViewById(R.id.lookupLocation);
+
+        String inputLocation_text = inputLocation.getText().toString();
+        lookupLocation.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.lookupLocation:
+
+                EditText inputLocation = findViewById(R.id.inputLocation);              //Reference to user's input text
+                String inputLocation_text = inputLocation.getText().toString();         //Grabbing said text
+
+                Intent intent1 = new Intent (this, MapsActivity.class);    //Creating intent to pass to MapActivity
+                intent1.putExtra("location",inputLocation_text);                    //Adding user input to intent
+                startActivity(intent1);                                                    //starting MapActivity with input
+        }
     }
 }
