d297e02c117bdf69b88db34d6396b631a9d1449d
Daniel Loi <dtloi@ucsc.edu>
2019-05-16 16:09:47 -0700

    Fixed some warnings

--- app/src/main/java/com/example/cmps121bdd/restroomfinder/addLocationDetails.java
+++ app/src/main/java/com/example/cmps121bdd/restroomfinder/addLocationDetails.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
+import android.nfc.Tag;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +21,7 @@ public class addLocationDetails extends AppCompatActivity implements View.OnClic
     EditText locName;
     Button locOpenMap;
     Button locCoor;
+    String tg = "addLocationDetails";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,7 +77,8 @@ public class addLocationDetails extends AppCompatActivity implements View.OnClic
         }
         catch (SecurityException e)
         {
-
+            e.printStackTrace();
+            Log.e(tg, "Security Exception: "+ e);
         }
     }
 }
